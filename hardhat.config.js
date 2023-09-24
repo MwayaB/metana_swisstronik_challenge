48497ecff22cb240012e93498b6a4950fdc1b7cd
Mwayanjana Bolokonya <mwayab@gmail.com>
2023-09-24 18:40:57 +0200

    added swisstronik testnet network to deploy contract to

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
+      accounts: [process.env.PRIVATE_KEY],
+    },}
 };
